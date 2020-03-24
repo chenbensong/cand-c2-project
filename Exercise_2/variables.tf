6f258f0706d05f453a278fda57be6b8d30cf0908
chenbensong <chenbensong@gmail.com>
2020-03-23 23:52:14 -0400

    Update variables.tf

--- Exercise_2/variables.tf
+++ Exercise_2/variables.tf
@@ -1,1 +1,10 @@
 # TODO: Define the variable for aws_region
+variable "region" {
+  default = "us-east-1"
+}
+
+# Lambda location
+variable "lambda_location" {
+  type    = list(string)
+  default = ["outputs/greet_lambda.zip"]
+}
