9a8ba7324d76cbac968cc57c72dde33528354c15
chenbensong <chenbensong@gmail.com>
2020-03-23 23:48:43 -0400

    Update main.tf

---
+++ Exercise_2/main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_iam_role" "iam_lambda" {
+  name = "iam_lambda"
+  assume_role_policy = <<EOF
+{
+	"Version": "2012-10-17",
+	"Statement": [{
+		"Action": "sts:AssumeRole",
+		"Principal": { "Service": "lambda.amazonaws.com" },
+		"Effect": "Allow",
+		"Sid": "" } ]
+}
+EOF
+}
+
+data "archive_file" "greet_lambda" {
+  type        = "zip"
+  source_file = "greet_lambda.py"
+  output_path = "var.lambda_location"
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  filename      = "var.lambda_location"
+  function_name = "greet_lambda"
+  role          = "${aws_iam_role.iam_lambda.arn}"
+  handler       = "greet_lambda.lambda_handler"
+
+  runtime = "python3.7"
+  environment {
+    variables = {
+      greeting = "Hello!"
+    }
+  }
+}
